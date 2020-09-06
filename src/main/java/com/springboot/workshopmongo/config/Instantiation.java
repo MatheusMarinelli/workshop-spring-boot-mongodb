113730077f4f378cff7b4f3092b2da73e9ac029b
matheus.marinelli <matheushlmarinelli@gmail.com>
2020-09-06 14:14:33 -0300

    Operação de instanciação do banco de dados

---
+++ src/main/java/com/springboot/workshopmongo/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.springboot.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.springboot.workshopmongo.domain.User;
+import com.springboot.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		//Limpa a coleção no mongoDB
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+	}
+
+}
