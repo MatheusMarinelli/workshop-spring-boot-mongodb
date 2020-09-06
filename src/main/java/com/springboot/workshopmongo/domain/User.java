8e5d0170863673e533971b345eb4cbc2fe3ecd47
matheus.marinelli <matheushlmarinelli@gmail.com>
2020-09-06 20:36:13 -0300

    Referenciando os posts do usuário

--- src/main/java/com/springboot/workshopmongo/domain/User.java
+++ src/main/java/com/springboot/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.springboot.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")
@@ -14,6 +17,9 @@ public class User implements Serializable{
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true) // evita trazer todos os posts do usuário
+	private List<Post> listPosts = new ArrayList<>();
 	
 	public User() {
 		
@@ -47,6 +53,14 @@ public class User implements Serializable{
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public List<Post> getListPosts() {
+		return listPosts;
+	}
+
+	public void setListPosts(List<Post> listPosts) {
+		this.listPosts = listPosts;
 	}
 
 	@Override
